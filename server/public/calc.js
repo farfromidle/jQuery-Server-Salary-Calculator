8ac45a4be23e04e98131d9baa5983e44f173bcce
Derrick Idleburg Jr <derrick@primeacademy.io>
2020-03-28 10:10:00 -0500

    setting up the index file and started working on the server side. Continuing from there on return

--- server/public/calc.js
+++ server/public/calc.js
@@ -1,7 +1,24 @@
 $(document).ready(init);
 
 console.log('server');
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null
+};
 
 function init() {
   console.log('Where are you running!?!');
 }
+
+function calcNum() {
+  let inputOne = $('firstNum').val();
+  let inputTwo = $('secondNum').val();
+}
+
+// Accept number inputs
+
+// calculate the numbers
+
+// clear, clear history?
